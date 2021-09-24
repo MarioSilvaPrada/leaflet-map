d54fadba923f4907b8d470661a5a82010b66a9b0
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-09-25 00:18:46 +0100

    update map

--- src/routes/App/index.tsx
+++ src/routes/App/index.tsx
@@ -1,39 +1,92 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
 import { getAllUsers } from 'api';
 import { IUser } from 'utils/interfaces';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvent,
+  useMap,
+} from 'react-leaflet';
 import * as S from './style';
+import { latLng } from 'leaflet';
 
-const App: FC = () => {
+interface IPos {
+  lat: number;
+  lng: number;
+}
+
+const FlyComponent = (props): null => {
+  const { lat, lng } = props;
+  const map = useMap();
+  if (lat) {
+    map.flyTo([lat, lng], 14, {
+      duration: 5,
+    });
+  }
+  return null;
+};
+
+const App: FC = (props) => {
+  const mapRef = useRef();
   const [mapUsers, setMapUsers] = useState<IUser[]>([]);
+
+  const [userPosition, setUserPosition] = useState<IPos>({
+    lat: 51.505,
+    lng: -0.09,
+  });
 
   const getUsers = async () => {
     const res = await getAllUsers();
-
     if (res) {
       setMapUsers(res);
     }
   };
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {}, [userPosition]);
+
+  const onCardClick = (lat: String, lng: string) => {
+    setUserPosition({
+      lat: Number(lat),
+      lng: Number(lng),
+    });
+  };
+
   return (
     <S.Container>
       <h1>Home</h1>
-      {mapUsers.map((user: IUser) => (
-        <div key={user.id}>
-          <p>{user.username}</p>
-          <p>{user.phone}</p>
-        </div>
-      ))}
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <S.CardsWrapper>
+        {mapUsers.map((user: IUser) => (
+          <S.UserCard
+            key={user.id}
+            onClick={() =>
+              onCardClick(user.address.geo.lat, user.address.geo.lng)
+            }
+          >
+            <p>{user.username}</p>
+            <p>{user.address.city}</p>
+            <p>{user.address.street}</p>
+            <p>{user.address.geo.lat}</p>
+            <p>{user.address.geo.lng}</p>
+          </S.UserCard>
+        ))}
+      </S.CardsWrapper>
+      <MapContainer
+        ref={mapRef}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={[userPosition.lat, userPosition.lng]} />
+        <FlyComponent lat={userPosition.lat} lng={userPosition.lng} />
       </MapContainer>
     </S.Container>
   );
--- src/routes/App/style.tsx
+++ src/routes/App/style.tsx
@@ -8,6 +8,14 @@ export const Title = styled.div`
   color: red;
 `;
 
-export const MapContainer = styled.div`
-  height: 180px;
+export const UserCard = styled.div`
+  border: 1px solid red;
+  cursor: pointer;
+  padding: 5px;
+  margin: 5px;
+`;
+
+export const CardsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
